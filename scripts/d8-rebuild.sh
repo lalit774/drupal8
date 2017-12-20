1aedfb93b93aba3a58a99f330780687f71e867ab
lkumar <lkumar@tvo.org>
2017-12-20 09:19:11 -0500

    create initializer module and update rebuild script.

--- scripts/d8-rebuild.sh
+++ scripts/d8-rebuild.sh
@@ -6,52 +6,42 @@
 reset_database(){
  # Refresh database from production
     echo Default settings in database
-    if [[ ! -f sites/default/settings.php ]];then
+    if [ ! -f sites/default/settings.php ];then
     echo Site name will be ${site_name}
-    drush site-install minimal -y --account-name=dev --account-pass=dev --db-url=mysql://${db_name}:${db_name}@v-swds-db01/${db_name} --site-name=TVOKids
-    # Install/Run TVO enable new structure module
-    drush en tvokids_enable_new_structure -y
-
-    # Install/Run TVO perform data module
-    drush en tvokids_perform_data -y
-
-    # Rebuild Permissions
-    drush eval 'node_access_rebuild();' 
+    drush si minimal -y --account-name=dev --account-pass=dev --db-url=mysql://${db_name}:${db_password}@localhost/${site_name} --site-name=site_name
+    # Install/Run initializer module
+    drush en initializer -y
     fi
-    drush vset error_level 2
     drush updb -y
-    drush cc views
     drush upwd dev --password="dev"
-    drush cc all
-    drush massimp
-    drush en tvokids_prod_content -y
+    drush cache-rebuild
     return 1;
 }
 #
 # Obtain environment
-current_DIR=$(readlink -f ../.)
+current_DIR=$(readlink -f ./.)
 current_DIR=${current_DIR##*/}
 environment=${current_DIR%%.*}
 
 # Verify environment pattern
-if [[ $environment != dev* ]];then
-  echo "Please execute script from a DEV environment"
+if [ ! $environment != drupal8* ];then
+  echo "Please execute script from a right project."
   exit
 fi
-echo 'done';
-exit
+
 site_name=environment
 # give permission to  settings.php
 # Delete settings.php
-chmod 777 sites/default
-chmod 777 sites/default/settings.php
-rm -f sites/default/settings.php
+if [ -f sites/default/settings.php ];then
+    chmod 777 sites/default
+    chmod 777 sites/default/settings.php
+    rm -f sites/default/settings.php
+fi
 
 
 # First time website setup
-db_name='org_'${environment}
-db_name=${db_name/x/}
-
+db_name='root'
+db_password='nothing'
 
 # Refresh git develop branch (or some other branch)i
 if [ -z "$1" ]; then
