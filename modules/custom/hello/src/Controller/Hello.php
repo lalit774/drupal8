ef92f90509953f95ed4fb5df34113f8942a49c5a
lkumar <#nidhi123456#>
2018-01-26 10:57:49 -0500

    Creating shell for drupal8
    create a form

--- modules/custom/hello/src/Controller/Hello.php
+++ modules/custom/hello/src/Controller/Hello.php
@@ -2,11 +2,41 @@
 
 namespace Drupal\hello\Controller;
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\hello\HelloContactStorage;
 /**
  * Greets the user.
  */
 class Hello extends ControllerBase {
     public function content() {
         return array('#markup' => $this->t('Hello world.'));
- }
+    }
+
+    /**
+     * Render a list of entries in the database.
+     */
+    public function entryList() {
+        $content = [];
+
+        $content['message'] = [
+            '#markup' => $this->t('Generate a list of all entries in the database. There is no filter in the query.'),
+        ];
+
+        $rows = [];
+        $headers = [t('Id'), t('uid'), t('Name'), t('Surname'), t('Age')];
+
+        foreach ($entries = HelloContactStorage::load() as $entry) {
+            // Sanitize each entry.
+            $rows[] = array_map('Drupal\Component\Utility\SafeMarkup::checkPlain', (array) $entry);
+        }
+        $content['table'] = [
+            '#type' => 'table',
+            '#header' => $headers,
+            '#rows' => $rows,
+            '#empty' => t('No entries available.'),
+        ];
+        // Don't cache this page.
+        $content['#cache']['max-age'] = 0;
+
+        return $content;
+    }
 }
